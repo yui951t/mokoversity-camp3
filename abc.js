b50bf603496851c1a52d8bcaabd1d6d98177a7a9
yui951t <yui951t@gmail.com>
2013-10-19 15:20:40 +0800

    fix. #7

--- abc.js
+++ abc.js
@@ -1,10 +1,12 @@
-(function() {
+var gameModule(function()){
+
+function star() {
         var canvas = document.getElementById('game');
         var ctx = canvas.getContext('2d');
 
     var ballX = Math.floor(Math.random() * 300),
         ballY = Math.floor(Math.random() * 500),
-        ballR = Math.floor(Math.random() * 100),
+        ballR = Math.floor(Math.random() * 100);
 
         canvas.width = 480;
         canvas.height = 320;
@@ -13,4 +15,10 @@
         ctx.beginPath();
         ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
         ctx.fill();
-}) ();
+    }
+    return(){
+        star:star;
+    }
+
+}) () ;
+gameModule.star();
